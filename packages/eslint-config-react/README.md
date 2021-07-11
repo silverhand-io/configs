28f756bfc5227511d95df67e632c8cb6654b6399
Gao Sun <gaosun.dev@gmail.com>
2021-07-11 16:09:10 +0800

    eslint: add react package

---
+++ packages/eslint-config-react/README.md
@@ -0,0 +1,11 @@
+# `@logto/eslint-config-react`
+
+> TODO: description
+
+## Usage
+
+```
+const eslintConfigReact = require('@logto/eslint-config-react');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,10 @@
+/** @type {import('eslint').Linter.BaseConfig} **/
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  extends: [
+    '@logto',
+    'xo-react/space',
+    // Make sure prettier is the last one
+    'plugin:prettier/recommended',
+  ],
+};
