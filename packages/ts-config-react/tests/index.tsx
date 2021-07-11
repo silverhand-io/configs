2d970384c16bf4832f145f4ca26ca9fe368d55cb
Gao Sun <gao@silverhand.io>
2021-07-11 16:44:29 +0800

    ts: add react config

---
+++ packages/ts-config-react/tests/index.tsx
@@ -0,0 +1,13 @@
+import bar from './FooBar';
+
+export type Optional<T> = T | undefined;
+
+const foo = async () => {
+  console.log(bar.aa);
+  [1].reduce((accumulator, aa) => accumulator + aa, 0);
+  return new Promise((resolve) => {
+    resolve(true);
+  });
+};
+
+void foo();
