1f2c546a7775f15a7dcce1a73a747fbfd6896b05
Gao Sun <gao@silverhand.io>
2021-07-14 00:42:29 +0800

    fix: use default export for replacements

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,11 +1,10 @@
 // Have to disable these rules to use CommonJS and enable VSCode ESLint plugin
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable unicorn/prefer-module */
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { replacements } = require('./replacements.js');
+const replacements = require('./replacements.js');
 
 /** @type {import('eslint').Linter.BaseConfig} **/
 module.exports = {
