bc721ba25592e59240bd4a89088d7e1eab8d5a54
Gao Sun <gaosun.dev@gmail.com>
2021-07-14 00:11:37 +0800

    eslint: inherit abbr replacements

---
+++ packages/eslint-config-react/replacements.js
@@ -0,0 +1,12 @@
+// Have to disable these rules to use CommonJS and enable VSCode ESLint plugin
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable unicorn/prefer-module */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { replacements } = require('@logto/eslint-config/replacements.js');
+
+module.exports = {
+  ...replacements,
+  src: false,
+};
---
+++ packages/eslint-config/replacements.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  replacements: {
+    env: false,
+    ctx: false,
+    i: false,
+    j: false,
+  },
+};
