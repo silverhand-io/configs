cb260e5d35c336bd9e336d1a3690581b726172d6
Gao Sun <gao@silverhand.io>
2021-07-26 00:09:20 +0800

    refactor: disable `unicorn/prefer-module` on config files

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,9 +1,4 @@
-// Have to disable these rules to use CommonJS and enable VSCode ESLint plugin
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable unicorn/prefer-module */
-/* eslint-disable @typescript-eslint/no-require-imports */
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 const replacements = require('./replacements.js');
 
 /** @type {import('eslint').Linter.BaseConfig} **/
