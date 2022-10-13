6800fcf5d0afe54a2eb48c1076a91485c16efd05
Gao Sun <gao@silverhand.io>
2022-10-13 12:41:19 +0800

    ci: add main workflow

--- packages/ts-config-react/tests/index.tsx
+++ packages/ts-config-react/tests/index.tsx
@@ -5,6 +5,7 @@ export type Optional<T> = T | undefined;
 const foo = async () => {
   console.log(bar.aa);
   [1].reduce((accumulator, aa) => accumulator + aa, 0);
+
   return new Promise((resolve) => {
     resolve(true);
   });
